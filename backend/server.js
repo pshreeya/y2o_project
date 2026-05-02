25dae93e9b440b53d03fce4cd3704e7f531a363c
Shreeya <pshreeya07@gmail.com>
2026-05-02 18:14:36 -0400

    correction

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
+
 const app = require("./api/index.js");
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Express server running on port ${PORT}`);
-});
+module.exports = app;
